16808cec24697ffeb14bfd4c49f66ecb0c4d6f49
Eric Yu <ericy27@gmail.com>
2026-05-15 23:36:58 -0700

    Update hash_function.cuh

--- include/utils/hash_function.cuh
+++ include/utils/hash_function.cuh
@@ -1,1 +1,10 @@
 #pragma once
+
+#define XXH_STATIC_LINKING_ONLY
+#define XXH_IMPLEMENTATION
+#define XXH_NO_STREAM
+#define XXH_CPU_LITTLE_ENDIAN 1
+#include "xxhash.h"
+
+#include <cuda.h>
+#include <cuda_runtime.h>
