74dcddf1cdf702df8d8932cfe6c5c70d3a7c2f3a
Eric Yu <ericy27@gmail.com>
2026-05-18 22:06:38 -0700

    Update cbf.cuh

--- include/bloom_filter/cbf.cuh
+++ include/bloom_filter/cbf.cuh
@@ -1,1 +1,15 @@
+#pragma once
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cstdint>
 #include "hash_function.cuh"
+
+// Allocate memory from host
+uint32_t* cbf_create(uint64_t m_bits) {
+    uint32_t* d_bits;
+    cudaMalloc(&d_bits, m_bits / 8);
+    cudaMemset(d_bits, 0, m_bits / 8);
+    return d_bits;
+}
+
+
