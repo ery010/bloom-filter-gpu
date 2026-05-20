e7eac280df4704f9c82807c72089275d7893baf8
Eric Yu <ericy27@gmail.com>
2026-05-20 01:32:19 -0700

    Update cbf.cu
    add host function defintion (header only)

--- include/bloom_filter/cbf.cu
+++ include/bloom_filter/cbf.cu
@@ -5,6 +5,9 @@
 
 __global__ void cbf_insert_kernel(uint64_t* d_bits, const uint64_t* d_keys, uint64_t n, uint32_t k, uint32_t num_words) {
     uint64_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid >= n) {
+        return;
+    };
 
     uint64_t key = d_keys[tid];
 }
@@ -18,5 +21,17 @@ ClassicalBloomFilter create_filter(uint32_t total_bits, uint32_t k) {
     filter.k_hashes = k;
 
     size_t total_bytes = filter.num_words * sizeof(uint64_t);
-    
+
+}
+
+void cbf_insert(ClassicalBloomFilter& filter, const uint64_t* d_keys, uint64_t n) {
+
+}
+
+void cbf_lookup(ClassicalBloomFilter& filter, const uint64_t* d_keys, uint64_t n, bool* d_results) {
+
+}
+
+void cbf_destroy(ClassicalBloomFilter& filter) {
+
 }
