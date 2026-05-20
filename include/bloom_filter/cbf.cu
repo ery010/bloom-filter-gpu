dcb2229551121a47d790ff0476f85a152caef73e
Eric Yu <ericy27@gmail.com>
2026-05-20 01:27:28 -0700

    Update cbf.cu
    added num_words calculation and k_hashes to cbf instance

--- include/bloom_filter/cbf.cu
+++ include/bloom_filter/cbf.cu
@@ -8,3 +8,15 @@ __global__ void cbf_insert_kernel(uint64_t* d_bits, const uint64_t* d_keys, uint
 
     uint64_t key = d_keys[tid];
 }
+
+// Host Wrappers
+
+ClassicalBloomFilter create_filter(uint32_t total_bits, uint32_t k) {
+    ClassicalBloomFilter filter;
+
+    filter.num_words = (total_bits + 63) / 64;
+    filter.k_hashes = k;
+
+    size_t total_bytes = filter.num_words * sizeof(uint64_t);
+    
+}
