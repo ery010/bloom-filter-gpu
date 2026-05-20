51ac4a09498436f919095fd146fef6d1587d4cff
Eric Yu <ericy27@gmail.com>
2026-05-19 23:17:58 -0700

    Update cbf.cuh

--- include/bloom_filter/cbf.cuh
+++ include/bloom_filter/cbf.cuh
@@ -4,18 +4,33 @@
 #include <cstdint>
 #include "hash_function.cuh"
 
-// Allocate memory from host
-uint64_t* cbf_create(uint64_t m_bits) {
+// CBF struct helper
+struct ClassicalBloomFilter {
     uint64_t* d_bits;
-    cudaMalloc(&d_bits, m_bits / 8);
-    cudaMemset(d_bits, 0, m_bits / 8);
-    return d_bits;
+    uint32_t num_words;
+    uint32_t k_hashes;
+
+};
+
+ClassicalBloomFilter create_filter(uint32_t total_bits, uint32_t k) {
+    ClassicalBloomFilter filter;
+
+    // Round to nearest 64-bit word
+    filter.num_words = (total_bits + 63) / 64;
+    filter.k_hashes = k;
+    
+    // Total bytes to allocate
+    size_t total_bytes = filter.num_words * sizeof(uint64_t);
+
+    cudaMalloc((void**)&filter.d_bits, total_bytes);
+    cudaMemset(filter.d_bits, 0, total_bytes);
+
+    return filter;
+
 }
 
-// Free up memory
-void cbf_destroy(uint64_t* d_bits) {
-    cudaFree(d_bits);
-}
+
+
 
 // Insert
 void cbf_insert(uint64_t* d_bits, const uint64_t* d_keys, uint64_t n, uint32_t k, uint32_t shift) {
@@ -25,4 +40,9 @@ void cbf_insert(uint64_t* d_bits, const uint64_t* d_keys, uint64_t n, uint32_t k
 // Lookup
 void cbf_lookup(uint64_t* d_bits, const uint64_t* d_keys, uint64_t n, bool* d_results, uint32_t k, uint32_t shift) {
 
+}
+
+// Free up memory
+void cbf_destroy(uint64_t* d_bits) {
+    cudaFree(d_bits);
 }
