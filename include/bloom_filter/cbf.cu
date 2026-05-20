75304da11c314b3aa4dbda57b6d1254e51121570
Eric Yu <ericy27@gmail.com>
2026-05-20 01:23:13 -0700

    Update cbf.cu

--- include/bloom_filter/cbf.cu
+++ include/bloom_filter/cbf.cu
@@ -1,2 +1,10 @@
 #include "cbf.cuh"
 #include "hash_function.cuh"
+
+// Device Kernels
+
+__global__ void cbf_insert_kernel(uint64_t* d_bits, const uint64_t* d_keys, uint64_t n, uint32_t k, uint32_t num_words) {
+    uint64_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    uint64_t key = d_keys[tid];
+}
