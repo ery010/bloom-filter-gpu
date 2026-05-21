98b478a843ee84d50adabe090343699f45d677fb
Eric Yu <ericy27@gmail.com>
2026-05-20 18:04:51 -0700

    Update cbf.cu
    - finished create_filter logic
    - added rounding to next power of 2
    - added shift logic using trailing zeros

--- include/bloom_filter/cbf.cu
+++ include/bloom_filter/cbf.cu
@@ -1,9 +1,10 @@
 #include "cbf.cuh"
 #include "hash_function.cuh"
+#include <bit>
 
 // Device Kernels
 
-__global__ void cbf_insert_kernel(uint64_t* d_bits, const uint64_t* d_keys, uint64_t n, uint32_t k, uint32_t num_words) {
+__global__ void cbf_insert_kernel(uint64_t* d_bits, const uint64_t* d_keys, uint64_t n, uint32_t k, uint32_t num_words, uint32_t shift) {
     uint64_t tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid >= n) {
         return;
@@ -15,13 +16,29 @@ __global__ void cbf_insert_kernel(uint64_t* d_bits, const uint64_t* d_keys, uint
 // Host Wrappers
 
 ClassicalBloomFilter create_filter(uint32_t total_bits, uint32_t k) {
-    ClassicalBloomFilter filter;
 
-    filter.num_words = (total_bits + 63) / 64;
+    // Round up to power of 2; can use std::bit_ceil() in C++20
+    uint32_t rounded_bits = total_bits;
+    if (rounded_bits > 0 && (rounded_bits & (rounded_bits - 1)) != 0) {
+        rounded_bits--;
+        rounded_bits |= rounded_bits >> 1;
+        rounded_bits |= rounded_bits >> 2;
+        rounded_bits |= rounded_bits >> 4;
+        rounded_bits |= rounded_bits >> 8;
+        rounded_bits |= rounded_bits >> 16;
+        rounded_bits++;
+    }
+
+    ClassicalBloomFilter filter;
+    filter.num_words = rounded_bits / 64;
     filter.k_hashes = k;
+    filter.shift = 64 - __builtin_ctz(filter.num_words); // log2(num_words), powers of 2 always have trailing zeros
 
     size_t total_bytes = filter.num_words * sizeof(uint64_t);
+    cudaMalloc(&filter.d_bits, total_bytes);
+    cudaMemset(filter.d_bits, 0, total_bytes);
 
+    return filter;
 }
 
 void cbf_insert(ClassicalBloomFilter& filter, const uint64_t* d_keys, uint64_t n) {
