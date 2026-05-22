db04cf52618f93c3a56c437827028d66062dec74
Eric Yu <ericy27@gmail.com>
2026-05-21 21:31:39 -0700

    Update bbf.cuh
    add

--- include/bloom_filter/bbf.cuh
+++ include/bloom_filter/bbf.cuh
@@ -12,3 +12,12 @@ struct BlockedBloomFilter {
 
 BlockedBloomFilter create_filter(uint64_t total_bits, uint32_t k, uint32_t num_blocks);
 
+// Insert
+void bbf_insert(BlockedBloomFilter& filter, const uint64_t* d_keys, uint64_t n);
+
+// Lookup
+void bbf_lookup(BlockedBloomFilter& filter, const uint64_t* d_keys, uint64_t n);
+
+// Free up memory
+void bbf_destroy(BlockedBloomFilter& filter);
+
