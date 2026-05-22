200b308279c580f3e4f416a80fa6aa8efcc6a9f8
Eric Yu <ericy27@gmail.com>
2026-05-22 00:03:21 -0700

    Update bbf.cu
    added rounding logic (identical to CBF)

--- include/bloom_filter/bbf.cu
+++ include/bloom_filter/bbf.cu
@@ -16,4 +16,24 @@
 
 // Host wrappers
 
+// Create bloom filter
+BlockedBloomFilter create_filter(uint64_t total_bits, uint32_t k, uint32_t num_blocks) {
+    // Round up to power of 2; can use std::bit_ceil() in C++20
+    
+    uint64_t rounded_bits = total_bits;
+    if (rounded_bits > 0 && (rounded_bits & (rounded_bits - 1)) != 0) {
+        rounded_bits--;
+        rounded_bits |= rounded_bits >> 1;
+        rounded_bits |= rounded_bits >> 2;
+        rounded_bits |= rounded_bits >> 4;
+        rounded_bits |= rounded_bits >> 8;
+        rounded_bits |= rounded_bits >> 16;
+        rounded_bits |= rounded_bits >> 32;
+        rounded_bits++;
+    }
+
+    BlockedBloomFilter filter;
+    
+}
+
 
