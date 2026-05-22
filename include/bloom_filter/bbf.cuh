b62066364f2c179d48a7dc3c7b4f13907dc5a359
Eric Yu <ericy27@gmail.com>
2026-05-21 20:21:25 -0700

    Update bbf.cuh

--- include/bloom_filter/bbf.cuh
+++ include/bloom_filter/bbf.cuh
@@ -1,5 +1,11 @@
 #pragma once
-#include <cuda.h>
-#include <cuda_runtime.h>
 #include <cstdint>
-#include <stdexcept>
+
+// BBF struct
+struct BlockBloomFilter {
+    uint64_t* d_bits;
+    uint32_t num_words;
+    uint32_t num_blocks; // BLOCK_SIZE = m (bit_array size) / b (num_blocks)
+    uint32_t k_hashes;
+    uint32_t shift;
+};
