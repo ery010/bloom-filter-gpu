89d61b770d954183d5e3ee09b44122b21652c46a
Eric Yu <ericy27@gmail.com>
2026-05-21 23:47:33 -0700

    Update bbf.cuh

--- include/bloom_filter/bbf.cuh
+++ include/bloom_filter/bbf.cuh
@@ -6,6 +6,7 @@ struct BlockedBloomFilter {
     uint64_t* d_bits;
     uint32_t num_words;
     uint32_t num_blocks; // BLOCK_SIZE = m (bit_array size) / b (num_blocks)
+    uint32_t words_per_block;
     uint32_t k_hashes;
     uint32_t shift;
 };
