2c81f712c61ed877782f86394ac5e61d45702fcf
Eric Yu <ericy27@gmail.com>
2026-05-21 21:20:51 -0700

    Update bbf.cuh

--- include/bloom_filter/bbf.cuh
+++ include/bloom_filter/bbf.cuh
@@ -2,10 +2,13 @@
 #include <cstdint>
 
 // BBF struct
-struct BlockBloomFilter {
+struct BlockedBloomFilter {
     uint64_t* d_bits;
     uint32_t num_words;
     uint32_t num_blocks; // BLOCK_SIZE = m (bit_array size) / b (num_blocks)
     uint32_t k_hashes;
     uint32_t shift;
 };
+
+BlockedBloomFilter create_filter(uint64_t total_bits, uint32_t k, uint32_t num_blocks);
+
